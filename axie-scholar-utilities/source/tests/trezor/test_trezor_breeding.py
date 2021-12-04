3bf08285f819d11d3b324a326914a8c191fa96e0
Oozman <hi@oozman.com>
2021-12-05 03:02:23 +0800

    Sync updates from v1.13
    https://github.com/FerranMarin/axie-scholar-utilities/compare/v1.12.1...v1.13

--- axie-scholar-utilities/source/tests/trezor/test_trezor_breeding.py
+++ axie-scholar-utilities/source/tests/trezor/test_trezor_breeding.py
@@ -4,23 +4,23 @@ import builtins
 
 from mock import patch, mock_open, call
 
-from axie import AxieBreedManager
-from axie.breeding import Breed, AXIE_CONTRACT
-from axie.payments import PaymentsSummary, CREATOR_FEE_ADDRESS
-from axie.utils import RONIN_PROVIDER_FREE
+from trezor import TrezorAxieBreedManager
+from trezor.trezor_breeding import TrezorBreed, AXIE_CONTRACT
+from axie.payments import CREATOR_FEE_ADDRESS, PaymentsSummary
+from axie.utils import RONIN_PROVIDER_FREE, USER_AGENT
 
 
-@patch("axie.breeding.load_json", return_value={"foo": "bar"})
+@patch("trezor.trezor_breeding.load_json", return_value={"foo": "bar"})
 def test_breed_manager_init(mock_load):
-    s_file = "_secrets_file.json"
+    c_file = "config_file.json"
     b_file = "breeds_file.json"
     acc = "ronin:abc1"
-    abm = AxieBreedManager(b_file, s_file, acc)
+    abm = TrezorAxieBreedManager(b_file, c_file, acc)
     mock_load.assert_has_calls(calls=[
-        call(s_file),
+        call(c_file),
         call(b_file)
     ])
-    assert abm.secrets == {"foo": "bar"}
+    assert abm.trezor_config == {"foo": "bar"}
     assert abm.breeding_file == {"foo": "bar"}
     assert abm.payment_account == acc
     assert abm.breeding_costs == 0
@@ -28,9 +28,9 @@ def test_breed_manager_init(mock_load):
 
 def test_breed_manager_verify_inputs_succcess(tmpdir):
     acc = 'ronin:<accountfoo_address>' + "".join([str(x) for x in range(10)]*4)
-    private_acc = '0x<accountfoo_private_address>012345' + "".join([str(x) for x in range(10)]*3)
-    s_file = tmpdir.join("s.json")
-    s_file.write('{"'+acc+'":"'+private_acc+'"}')
+    c_file = tmpdir.join("c.json")
+    config_data = {acc: {"passphrase": "", "bip_path": "m/44'/60'/0'/0/0"}}
+    c_file.write(json.dumps(config_data))
     b_file = tmpdir.join("b.json")
     data = [{
         "Sire": 1234,
@@ -38,16 +38,16 @@ def test_breed_manager_verify_inputs_succcess(tmpdir):
         "AccountAddress": acc
     }]
     b_file.write(json.dumps(data))
-    abm = AxieBreedManager(b_file, s_file, acc)
+    abm = TrezorAxieBreedManager(b_file, c_file, acc)
     abm.verify_inputs()
-    assert abm.secrets == {acc: private_acc}
+    assert abm.trezor_config == config_data
 
 
 def test_breed_manager_verify_inputs_fail_validation(tmpdir, caplog):
     acc = '0x<accountfoo_address>' + "".join([str(x) for x in range(10)]*4)
-    private_acc = '0x<accountfoo_private_address>012345' + "".join([str(x) for x in range(10)]*3)
-    s_file = tmpdir.join("s.json")
-    s_file.write('{"'+acc+'":"'+private_acc+'"}')
+    c_file = tmpdir.join("c.json")
+    config_data = {acc: {"passphrase": "", "bip_path": "m/44'/60'/0'/0/0"}}
+    c_file.write(json.dumps(config_data))
     b_file = tmpdir.join("b.json")
     data = [{
         "Sire": 1234,
@@ -56,16 +56,17 @@ def test_breed_manager_verify_inputs_fail_validation(tmpdir, caplog):
     }]
     b_file.write(json.dumps(data))
     with patch.object(sys, "exit") as mocked_sys:
-        abm = AxieBreedManager(b_file, s_file, acc)
+        abm = TrezorAxieBreedManager(b_file, c_file, acc)
         abm.verify_inputs()
     mocked_sys.assert_called_once()
     assert f"Validation of breeding file failed. Error given: '{acc}' does not match '^ronin:'" in caplog.text
 
 
 def test_breed_manager_verify_inputs_missing_account_in_secrets(tmpdir, caplog):
-    acc = '0x<accountfoo_address>' + "".join([str(x) for x in range(10)]*4)
-    s_file = tmpdir.join("s.json")
-    s_file.write('{"foo":"bar"}')
+    acc = 'ronin:<accountfoo_address>' + "".join([str(x) for x in range(10)]*4)
+    c_file = tmpdir.join("c.json")
+    config_data = {"ronin:123": {"passphrase": "", "bip_path": "m/44'/60'/0'/0/0"}}
+    c_file.write(json.dumps(config_data))
     b_file = tmpdir.join("b.json")
     data = [{
         "Sire": 1234,
@@ -74,17 +75,17 @@ def test_breed_manager_verify_inputs_missing_account_in_secrets(tmpdir, caplog):
     }]
     b_file.write(json.dumps(data))
     with patch.object(sys, "exit") as mocked_sys:
-        abm = AxieBreedManager(b_file, s_file, acc)
+        abm = TrezorAxieBreedManager(b_file, c_file, acc)
         abm.verify_inputs()
     mocked_sys.assert_called_once()
-    assert f"Account '{acc}' is not present in secret file, please add it." in caplog.text
+    assert f"Account '{acc}' is not present in trezor config, please re-run setup." in caplog.text
 
 
 def test_breed_manager_verify_inputs_missing_payments_account_in_secrets(tmpdir, caplog):
-    acc = '0x<accountfoo_address>' + "".join([str(x) for x in range(10)]*4)
-    private_acc = '0x<accountfoo_private_address>012345' + "".join([str(x) for x in range(10)]*3)
-    s_file = tmpdir.join("s.json")
-    s_file.write('{"'+acc+'":"'+private_acc+'"}')
+    acc = 'ronin:<accountfoo_address>' + "".join([str(x) for x in range(10)]*4)
+    c_file = tmpdir.join("c.json")
+    config_data = {acc: {"passphrase": "", "bip_path": "m/44'/60'/0'/0/0"}}
+    c_file.write(json.dumps(config_data))
     b_file = tmpdir.join("b.json")
     data = [{
         "Sire": 1234,
@@ -93,19 +94,19 @@ def test_breed_manager_verify_inputs_missing_payments_account_in_secrets(tmpdir,
     }]
     b_file.write(json.dumps(data))
     with patch.object(sys, "exit") as mocked_sys:
-        abm = AxieBreedManager(b_file, s_file, "ronin:abc1")
+        abm = TrezorAxieBreedManager(b_file, c_file, "ronin:abc1")
         abm.verify_inputs()
     mocked_sys.assert_called_once()
-    assert "Payment account 'ronin:abc1' is not present in secret file, please add it." in caplog.text
+    assert "Payment account 'ronin:abc1' is not present in trezor config, please re-run setup." in caplog.text
 
 
-@patch("axie.AxieBreedManager.calculate_fee_cost")
-@patch("axie.AxieBreedManager.calculate_breeding_cost")
+@patch("trezor.TrezorAxieBreedManager.calculate_fee_cost")
+@patch("trezor.TrezorAxieBreedManager.calculate_breeding_cost")
 def test_breed_manager_calculate_cost(mock_breed_cost, mock_fee_cost, tmpdir):
     acc = 'ronin:<accountfoo_address>' + "".join([str(x) for x in range(10)]*4)
-    private_acc = '0x<accountfoo_private_address>012345' + "".join([str(x) for x in range(10)]*3)
-    s_file = tmpdir.join("s.json")
-    s_file.write('{"'+acc+'":"'+private_acc+'"}')
+    c_file = tmpdir.join("c.json")
+    config_data = {acc: {"passphrase": "", "bip_path": "m/44'/60'/0'/0/0"}}
+    c_file.write(json.dumps(config_data))
     b_file = tmpdir.join("b.json")
     data = [{
         "Sire": 1234,
@@ -113,7 +114,7 @@ def test_breed_manager_calculate_cost(mock_breed_cost, mock_fee_cost, tmpdir):
         "AccountAddress": acc
     }]
     b_file.write(json.dumps(data))
-    abm = AxieBreedManager(b_file, s_file, acc)
+    abm = TrezorAxieBreedManager(b_file, c_file, acc)
     abm.calculate_cost()
     mock_breed_cost.assert_not_called()
     mock_fee_cost.assert_called_once()
@@ -121,9 +122,9 @@ def test_breed_manager_calculate_cost(mock_breed_cost, mock_fee_cost, tmpdir):
 
 def test_breed_manager_calculate_fee_cost(tmpdir):
     acc = 'ronin:<accountfoo_address>' + "".join([str(x) for x in range(10)]*4)
-    private_acc = '0x<accountfoo_private_address>012345' + "".join([str(x) for x in range(10)]*3)
-    s_file = tmpdir.join("s.json")
-    s_file.write('{"'+acc+'":"'+private_acc+'"}')
+    c_file = tmpdir.join("c.json")
+    config_data = {acc: {"passphrase": "", "bip_path": "m/44'/60'/0'/0/0"}}
+    c_file.write(json.dumps(config_data))
     b_file = tmpdir.join("b.json")
     data = [{
         "Sire": 1234,
@@ -131,7 +132,7 @@ def test_breed_manager_calculate_fee_cost(tmpdir):
         "AccountAddress": acc
     }]
     b_file.write(json.dumps(data))
-    abm = AxieBreedManager(b_file, s_file, acc)
+    abm = TrezorAxieBreedManager(b_file, c_file, acc)
     assert abm.calculate_fee_cost() == 30
     abm.breeding_file = [1, 2, 3]
     assert abm.calculate_fee_cost() == 30*3
@@ -145,9 +146,9 @@ def test_breed_manager_calculate_fee_cost(tmpdir):
 
 def test_breed_manager_calculate_breeding_cost(tmpdir):
     acc = 'ronin:<accountfoo_address>' + "".join([str(x) for x in range(10)]*4)
-    private_acc = '0x<accountfoo_private_address>012345' + "".join([str(x) for x in range(10)]*3)
-    s_file = tmpdir.join("s.json")
-    s_file.write('{"'+acc+'":"'+private_acc+'"}')
+    c_file = tmpdir.join("c.json")
+    config_data = {acc: {"passphrase": "", "bip_path": "m/44'/60'/0'/0/0"}}
+    c_file.write(json.dumps(config_data))
     b_file = tmpdir.join("b.json")
     data = [{
         "Sire": 1234,
@@ -155,27 +156,31 @@ def test_breed_manager_calculate_breeding_cost(tmpdir):
         "AccountAddress": acc
     }]
     b_file.write(json.dumps(data))
-    abm = AxieBreedManager(b_file, s_file, acc)
+    abm = TrezorAxieBreedManager(b_file, c_file, acc)
     assert abm.calculate_breeding_cost() == 0
     abm.breeding_file = list(range(75))
     assert abm.calculate_breeding_cost() == 0
 
 
-@patch("axie.breeding.check_balance", return_value=1000)
-@patch("axie.breeding.Breed.execute")
-@patch("axie.breeding.Breed.__init__", return_value=None)
-@patch("axie.payments.Payment.execute")
-@patch("axie.payments.Payment.__init__", return_value=None)
+@patch("trezor.trezor_breeding.parse_path", return_value="parsed_path")
+@patch("trezor.trezor_breeding.get_default_client", return_value='client')
+@patch("trezor.trezor_breeding.check_balance", return_value=1000)
+@patch("trezor.trezor_breeding.TrezorBreed.execute")
+@patch("trezor.trezor_breeding.TrezorBreed.__init__", return_value=None)
+@patch("trezor.trezor_payments.TrezorPayment.execute")
+@patch("trezor.trezor_payments.TrezorPayment.__init__", return_value=None)
 def test_breed_manager_execute(mock_payments_init,
                                mock_payments_execute,
                                mock_breed_init,
                                mock_bree_execute,
                                mock_check_balance,
+                               mocked_client,
+                               mock_parse,
                                tmpdir):
     acc = 'ronin:<accountfoo_address>' + "".join([str(x) for x in range(10)]*4)
-    private_acc = '0x<accountfoo_private_address>012345' + "".join([str(x) for x in range(10)]*3)
-    s_file = tmpdir.join("s.json")
-    s_file.write('{"'+acc+'":"'+private_acc+'"}')
+    c_file = tmpdir.join("c.json")
+    config_data = {acc: {"passphrase": "", "bip_path": "m/44'/60'/0'/0/0"}}
+    c_file.write(json.dumps(config_data))
     b_file = tmpdir.join("b.json")
     data = [{
         "Sire": 1234,
@@ -187,35 +192,39 @@ def test_breed_manager_execute(mock_payments_init,
         "AccountAddress": acc
     }]
     b_file.write(json.dumps(data))
-    abm = AxieBreedManager(b_file, s_file, acc)
+    abm = TrezorAxieBreedManager(b_file, c_file, acc)
     abm.execute()
+    mocked_client.assert_called()
+    mock_parse.assert_called()
     mock_check_balance.assert_called_once()
     mock_breed_init.assert_has_calls(calls=[
-        call(sire_axie=1234, matron_axie=5678, address=acc, private_key=private_acc),
-        call(sire_axie=123, matron_axie=456, address=acc, private_key=private_acc)
+        call(sire_axie=1234, matron_axie=5678, address=acc, client="client", bip_path="m/44'/60'/0'/0/0"),
+        call(sire_axie=123, matron_axie=456, address=acc, client="client", bip_path="m/44'/60'/0'/0/0")
     ])
     assert mock_bree_execute.call_count == 2
     mock_payments_init.assert_called_with(
         "Breeding Fee",
         "donation",
+        "client",
+        "parsed_path",
         acc,
-        private_acc,
         CREATOR_FEE_ADDRESS,
         60,
         PaymentsSummary())
     assert mock_payments_execute.call_count == 1
 
 
-@patch("axie.payments.Payment.execute")
-@patch("axie.breeding.Breed.execute")
-@patch("axie.payments.Payment.__init__", return_value=None)
-@patch("axie.breeding.Breed.__init__", return_value=None)
-@patch("axie.breeding.check_balance", return_value=0)
-def test_breed_manager_execute_not_enough_slp(mock_check_balance, _, __, ___, ____, tmpdir, caplog):
+@patch("trezor.trezor_breeding.get_default_client", return_value='client')
+@patch("trezor.trezor_payments.TrezorPayment.execute")
+@patch("trezor.trezor_breeding.TrezorBreed.execute")
+@patch("trezor.trezor_payments.TrezorPayment.__init__", return_value=None)
+@patch("trezor.trezor_breeding.TrezorBreed.__init__", return_value=None)
+@patch("trezor.trezor_breeding.check_balance", return_value=0)
+def test_breed_manager_execute_not_enough_slp(mock_check_balance, _, __, ___, ____, mocked_client, tmpdir, caplog):
     acc = 'ronin:<accountfoo_address>' + "".join([str(x) for x in range(10)]*4)
-    private_acc = '0x<accountfoo_private_address>012345' + "".join([str(x) for x in range(10)]*3)
-    s_file = tmpdir.join("s.json")
-    s_file.write('{"'+acc+'":"'+private_acc+'"}')
+    c_file = tmpdir.join("c.json")
+    config_data = {acc: {"passphrase": "", "bip_path": "m/44'/60'/0'/0/0"}}
+    c_file.write(json.dumps(config_data))
     b_file = tmpdir.join("b.json")
     data = [{
         "Sire": 1234,
@@ -227,30 +236,34 @@ def test_breed_manager_execute_not_enough_slp(mock_check_balance, _, __, ___, __
         "AccountAddress": acc
     }]
     b_file.write(json.dumps(data))
-    abm = AxieBreedManager(b_file, s_file, acc)
+    abm = TrezorAxieBreedManager(b_file, c_file, acc)
     with patch.object(sys, "exit") as mocked_sys:
         abm.execute()
     mocked_sys.assert_called_once()
     mock_check_balance.assert_called_once()
+    mocked_client.assert_called()
     assert "Not enough SLP funds to pay for breeding and the fee" in caplog.text
 
 
-def test_breed_init():
+@patch("trezor.trezor_breeding.parse_path", return_value="parsed_path")
+def test_breed_init(mock_parse):
     acc = 'ronin:<accountfoo_address>' + "".join([str(x) for x in range(10)]*4)
-    private_acc = '0x<accountfoo_private_address>012345' + "".join([str(x) for x in range(10)]*3)
-    b = Breed(sire_axie=123, matron_axie=456, address=acc, private_key=private_acc)
+    b = TrezorBreed(sire_axie=123, matron_axie=456, address=acc, client='client', bip_path="m/44'/60'/0'/0/0")
     assert b.sire_axie == 123
     assert b.matron_axie == 456
-    assert b.private_key == private_acc
+    assert b.client == "client"
     assert b.address == acc.replace("ronin:", "0x")
+    assert b.bip_path == "parsed_path"
 
 
+@patch("trezor.trezor_breeding.rlp.encode")
+@patch("web3.Web3.toBytes")
 @patch("web3.Web3.toHex", return_value="transaction_hash")
 @patch("web3.Web3.keccak", return_value='result_of_keccak')
 @patch("web3.eth.Eth.get_transaction_receipt", return_value={'status': 1})
 @patch("web3.eth.Eth.send_raw_transaction", return_value="raw_tx")
-@patch("web3.eth.Eth.account.sign_transaction")
-@patch("axie.breeding.get_nonce", return_value=1)
+@patch('trezor.trezor_breeding.ethereum.sign_tx')
+@patch("trezor.trezor_breeding.get_nonce", return_value=1)
 @patch("web3.eth.Eth.contract")
 @patch("web3.Web3.toChecksumAddress", return_value="checksum")
 @patch("web3.Web3.HTTPProvider", return_value="provider")
@@ -262,16 +275,22 @@ def test_breed_execute(mocked_provider,
                        mock_raw_send,
                        mock_receipt,
                        mock_keccak,
-                       mock_to_hex):
+                       mock_to_hex,
+                       mocked_to_bytes,
+                       mock_rlp):
     acc = 'ronin:<accountfoo_address>' + "".join([str(x) for x in range(10)]*4)
-    private_acc = '0x<accountfoo_private_address>012345' + "".join([str(x) for x in range(10)]*3)
     with patch.object(builtins,
                       "open",
                       mock_open(read_data='{"foo": "bar"}')):
-        b = Breed(sire_axie=123, matron_axie=456, address=acc, private_key=private_acc)
+        b = TrezorBreed(sire_axie=123, matron_axie=456, address=acc, client='client', bip_path="m/44'/60'/0'/0/0")
         b.execute()
+    mocked_to_bytes.assert_called()
+    mock_rlp.assert_called()
     mock_get_nonce.assert_called_once()
-    mocked_provider.assert_called_with(RONIN_PROVIDER_FREE)
+    mocked_provider.assert_called_with(
+        RONIN_PROVIDER_FREE,
+        request_kwargs={"headers":{"content-type":"application/json","user-agent": USER_AGENT}}
+    )
     mocked_checksum.assert_called_with(AXIE_CONTRACT)
     mocked_contract.assert_called_with(address="checksum", abi={"foo": "bar"})
     mocked_sign_transaction.assert_called_once()
